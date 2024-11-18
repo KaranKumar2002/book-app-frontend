e4ac2a3c7c23c10254a4b063879fd65830a41d19
Karan <karan.kumar800433@gmail.com>
2024-11-18 16:35:07 +0530

    first commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Banner from './Banner'
-import TopSellers from './topSellers'
+import TopSellers from './TopSellers'
 import Recommend from './Recommend'
 import News from './News'
 
